a50f509e7685e7177f634acd469725815c6440a8
Saurabh Nutan Sohoni <ssohon@sapient.com>
2018-03-02 20:50:45 +0530

    Reduced batch size to 5

--- main.py
+++ main.py
@@ -163,7 +163,7 @@ def run():
 
         # TODO: Train NN using the train_nn function
         num_epochs = 5
-        batch_size = 50
+        batch_size = 5
 
         correct_label = tf.placeholder(tf.int32, [None, None, None, num_classes], name='correct_label')
         learning_rate = tf.placeholder(tf.float32, name='learning_rate')
