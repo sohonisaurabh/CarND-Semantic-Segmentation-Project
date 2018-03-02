a0cf4f7cd3f5d27c96da8f3731d7108608688090
Saurabh Nutan Sohoni <ssohon@sapient.com>
2018-03-02 14:14:37 +0530

    Added regularization loss

--- main.py
+++ main.py
@@ -94,10 +94,13 @@ def optimize(nn_last_layer, correct_label, learning_rate, num_classes):
 
     logits = nn_last_layer
     cross_entropy_loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits, correct_label))
+    reg_losses = tf.get_collection(tf.GraphKeys.REGULARIZATION_LOSSES)
+    reg_constant = 1  # Choose an appropriate one.
+    total_loss = cross_entropy_loss + reg_constant * sum(reg_losses)
     optimizer = tf.train.AdamOptimizer(learning_rate= learning_rate)
-    train_op = optimizer.minimize(cross_entropy_loss)
+    train_op = optimizer.minimize(total_loss)
 
-    return logits, cross_entropy_loss, train_op
+    return logits, total_loss, train_op
 tests.test_optimize(optimize)
 
 
