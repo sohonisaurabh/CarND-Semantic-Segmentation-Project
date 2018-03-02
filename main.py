ec68c10f5e889a9e9ffce09fad9ef8fa02c389ee
Saurabh Sohoni <sohonisaurabh@ymail.com>
2018-03-02 07:02:14 -0800

    removed errors

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ def load_vgg(sess, vgg_path):
     tf.saved_model.loader.load(sess, [vgg_tag], vgg_path)
     default_graph = tf.get_default_graph()
 
-    input_layer_out = default_graph.get_tensor_by_name(vgg_input_tensor_name)
+    input_image = default_graph.get_tensor_by_name(vgg_input_tensor_name)
     keep_prob = default_graph.get_tensor_by_name(vgg_keep_prob_tensor_name)
     layer3_out = default_graph.get_tensor_by_name(vgg_layer3_out_tensor_name)
     layer4_out = default_graph.get_tensor_by_name(vgg_layer4_out_tensor_name)
