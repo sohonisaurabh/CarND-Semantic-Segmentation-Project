5da9f92663bbd1e8ee2c2414caf5370f6236cfd6
Saurabh Nutan Sohoni <ssohon@sapient.com>
2018-03-03 11:25:08 +0530

    Trying for minimal loss

--- main.py
+++ main.py
@@ -181,7 +181,7 @@ def run():
                     correct_label, keep_prob, learning_rate)
 
         # TODO: Save inference data using helper.save_inference_samples
-        helper.save_inference_samples(runs_dir, data_dir, sess, image_shape, logits, keep_prob, input_image)
+        #helper.save_inference_samples(runs_dir, data_dir, sess, image_shape, logits, keep_prob, input_image)
 
         # OPTIONAL: Apply the trained model to a video
 
